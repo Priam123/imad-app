523f2d72f5868376937f243ace5a86817a9f73f4
Priam123 <13pramo@gmail.com>
2018-02-27 23:00:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,125 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles =  {
+    articleOne : {
+    title: "Article One | Pramothini",
+    heading: "Article One",
+    date: "Feb 26, 2018",
+    content:
+                `<p>
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                </p>
+                <p>
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                </p>
+                <p>
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                    This is the content for article one. This is the content for article one. This is the content for article one.
+                </p>
+                `
+},
+
+     articleTwo: {
+    title: "Article Two | Pramothini",
+    heading: "Article Two",
+    date: "Feb 20, 2018",
+    content:
+        ` <p>
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                </p>
+                <p>
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                </p>
+                <p>
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                    This is the content for article two. This is the content for article two. This is the content for article two.
+                </p>`
+                
+},
+    articleThree: {
+    title: "Article Three | Pramothini",
+    heading: "Article Three",
+    date: "Feb 15, 2018",
+    content:
+                `
+                <p>
+                    This is the content for article three. This is the content for article three. This is the content for article three.
+                    This is the content for article three. This is the content for article three. This is the content for article three.
+                </p>
+                <p>
+                    This is the content for article three. This is the content for article three. This is the content for article three.
+                    This is the content for article three. This is the content for article three. This is the content for article three.
+                </p>
+                <p>
+                    This is the content for article three. This is the content for article three. This is the content for article three.
+                    This is the content for article three. This is the content for article three. This is the content for article three.
+                </p>
+                `
+}
+};
+
+function createTemplate(data){
+    
+    var title = data.title
+    var heading = data.heading
+    var date = data.date
+    var content = date.content
+    
+    var htmlTemplate = 
+    `<html>
+        <head>
+            <title>${title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>`
+;
+
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(articleTwo));
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.send(createTemplate(articleThree));
 });
 
 app.get('/ui/style.css', function (req, res) {
